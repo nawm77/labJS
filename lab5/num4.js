7205b390eebd8c127d55cdef45f70d7b849863fa
nawm77 <alpha050603@gmail.com>
2024-02-23 23:44:45 +0300

    add num 4 for lab 5

--- lab5/num4.js
+++ lab5/num4.js
@@ -15,12 +15,59 @@ function defineAction(command) {
 }
 
 function process(commandArray) {
-    let map = new Map()
+    let list = [];
     for (let i = 0; i < commandArray.length; i++) {
-        let command = commandArray[i]
-        let action = defineAction(command)
-        if(action === ACTION_ADD) {
-            map.set()
+        let command = commandArray[i].split(" ");
+        let action = defineAction(command);
+        if (action === ACTION_ADD) {
+            let name = command.slice(1, command.length).join(" ");
+            let movie = new Movie(name);
+            list.push(movie);
+        }
+        if (action === ACTION_DIRECTED_BY) {
+            let name = command.slice(0, command.indexOf("directedBy")).join(" ").trim();
+            console.log(name)
+            let existingMovie = list.find(movie => movie.name === name);
+            let director = command.slice(command.indexOf("directedBy") + 1).join(" ");
+            if (existingMovie) {
+                existingMovie.director = director;
+            } else {
+                let movie = new Movie(name, undefined, director);
+                list.push(movie);
+            }
+        }
+        if (action === ACTION_ON_DATE) {
+            let name = command.slice(0, command.indexOf("onDate")).join(" ");
+            let existingMovie = list.find(movie => movie.name === name);
+            let date = command[command.length - 1];
+            if (existingMovie) {
+                existingMovie.date = date;
+            } else {
+                let movie = new Movie(name, date);
+                list.push(movie);
+            }
         }
     }
+    list.filter(m => m.director !== undefined && m.name !== undefined && m.date !== undefined)
+        .forEach(m => console.log(JSON.stringify(m)));
 }
+
+
+class Movie {
+    constructor(name = undefined, date = undefined, director = undefined) {
+        this.name = name;
+        this.date = date;
+        this.director = director;
+    }
+}
+
+let commands = ['addMovie Fast and Furious',
+    'addMovie Godfather',
+    'Inception directedBy Christopher Nolan',
+    'Godfather directedBy Francis Ford Coppola',
+    'Godfather onDate 29.07.2018',
+    'Fast and Furious onDate 30.07.2018',
+    'Batman onDate 01.08.2018',
+    'Fast and Furious directedBy Rob Cohen']
+
+process(commands)
